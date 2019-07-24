30aa0a3cb162e3a2d2399dd20dc581d000aa032c
Yxiao <yxiao220@gmail.com>
2019-07-23 23:22:28 -0400

    fix

--- ios/RCTWeiboAPI/RCTWeiboAPI.h
+++ ios/RCTWeiboAPI/RCTWeiboAPI.h
@@ -6,7 +6,12 @@
 //  Copyright © 2016 erica. All rights reserved.
 //
 
+
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
 #import "RCTBridgeModule.h"
+#endif
 
 @interface RCTWeiboAPI : NSObject<RCTBridgeModule>
 
