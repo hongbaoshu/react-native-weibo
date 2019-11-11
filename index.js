66e428131b005e83210fcfd03c8f907621c72d28
Yxiao <yxiao220@gmail.com>
2019-11-11 01:09:31 -0500

    fix

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const defaultRedirectURI = "https://api.weibo.com/oauth2/default.html";
 
 export const login = WeiboAPI.login;
 export const share = WeiboAPI.share;
-export const register = ({ appId, redirectUri = defaultRedirectURI, scope = defaultScope }) => WeiboAPI.register({
+export const register = ({ appId, redirectURI = defaultRedirectURI, scope = defaultScope }) => WeiboAPI.register({
   appId,
-  redirectUri,
+  redirectURI,
   scope
 });
