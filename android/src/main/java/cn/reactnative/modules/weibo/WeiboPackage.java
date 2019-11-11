277d2b1ff1c715071017f7ab9156476eae8fe261
Yxiao <yxiao220@gmail.com>
2019-11-10 21:59:16 -0500

    work on android weibo login

--- android/src/main/java/cn/reactnative/modules/weibo/WeiboPackage.java
+++ android/src/main/java/cn/reactnative/modules/weibo/WeiboPackage.java
@@ -6,20 +6,13 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Created by lvbingru on 12/22/15.
- */
 public class WeiboPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.asList(new NativeModule[]{
-                // Modules from third-party
-                new WeiboModule(reactContext),
-        });
+        return Collections.singletonList(new WeiboModule(reactContext));
     }
 
     public List<Class<? extends JavaScriptModule>> createJSModules() {
